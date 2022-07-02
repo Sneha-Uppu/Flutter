52047b33b32199de3b7aee119a0f6ae45dbe15ed
Sneha-Uppu <81636785+Sneha-Uppu@users.noreply.github.com>
2022-07-02 15:08:01 +0530

    first commit

---
+++ Practice/4.dart
@@ -0,0 +1,46 @@
+//Rows- Displaying 3 widgets in a row like a table
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget{
+  @override 
+  Widget build(BuildContext context){
+    return Scaffold(
+    appBar: AppBar(
+      title: Text('Practice App'),
+      centerTitle: true,
+      backgroundColor: Colors.deepOrange[800],
+    ),
+    body: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Container(
+          color: Color.fromARGB(255, 253, 85, 141),
+          padding: EdgeInsets.all(30.0),
+          child: Text('Roll No'),
+        ),
+        Container(
+          color: Colors.cyan,
+          padding: EdgeInsets.all(30.0),
+          child: Text('Name'),
+        ),
+        Container(
+          color: Color.fromARGB(255, 218, 96, 239),
+          padding: EdgeInsets.all(30.0),
+          child: Text('Hobies'),
+        )
+      ],
+      ),
+    floatingActionButton: FloatingActionButton(
+      onPressed: () {},
+      child: Text('Like'),
+      backgroundColor: Colors.deepOrange[800],
+    ),
+  );
+  }
+}
